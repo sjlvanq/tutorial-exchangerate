4446c0b5f446251d15eddf31af5f44844dcbdec1
sjlvanq <sjlvanq@gmail.com>
2025-05-18 08:21:50 -0300

    fix explanatory comments

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 // ¿Cuál es la funcionalidad esencial de nuestro programa?
-// -- Conectarse a un recurso de internet y obtener la respuesta
+// -- Consultar un recurso de internet y obtener una respuesta
 
 // * El cliente envía una solicitud a un servidor
 // * El servidor procesa la solicitud y devuelve una respuesta
@@ -18,8 +18,9 @@ public class Main{
 
         // Nuestra solicitud se hará al servidor lode.uno,
         // le pediremos el recurso holamundo.txt"
-        // Al siguiente String se lo pasaremos luego al
-        // métod0 de HttpClient que construye un objeto
+
+        // Al valor de la variable url de tipo String
+        // la usaremos luego para construir un objeto
         // del tipo solicitud HttpRequest.
         String url = "https://lode.uno/holamundo.txt";
 
@@ -28,27 +29,28 @@ public class Main{
         // como un objeto HttpResponse.
         HttpClient client = HttpClient.newHttpClient();
 
-        // El Request es la solicitud que envía el Cliente
+        // Contruimos el objeto HttpRequest que representa
+        // la solicitud que enviaremos mediante el Cliente
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .build();
 
-        // Aquí se envía la solicitud y se almacena la
-        // respuesta en una variable.
-        // Invocamos al métod0 send que devuelve un objeto
-        // de tipo respuesta HttpResponse.
+        // Enviamos la solicitud y almacenamos la
+        // respuesta en la variable response del tipo
+        // HttpResponse.
+        // Invocamos al métod0 send() del objeto Cliente.
         HttpResponse<String> response = client
             .send(request, HttpResponse.BodyHandlers.ofString());
 
         // Aquí ya hicimos la solicitud y cerramos el cliente
         // para que deje de ocupar memoria y recursos.
-        // Invocamos al métod0 close() del objeto HttpClient.
+        // Invocamos al métod0 close() del objeto Cliente.
         client.close();
 
         // La respuesta tendrá un encabezado con información
-        // acerca de cómo el servidor manejó la consulta.
-        // y un cuerpo que será la información que solicitamos.
-        // Invocamos al métod0 body() del objeto HttpResponse.
+        // acerca de cómo el servidor manejó la consulta
+        // y un cuerpo que contendrá la respuesta en sí.
+        // Invocamos al métod0 body() del objeto Response.
         responseBody = response.body();
 
         System.out.println(responseBody);
