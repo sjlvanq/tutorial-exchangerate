62efea4286c8532c46bcee5f0f72a9f2355b1687
sjlvanq <sjlvanq@gmail.com>
2025-05-17 21:03:00 -0300

    initial commit 02-conexion-ii

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,59 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-// ¿Cuál es la funcionalidad esencial de nuestro programa?
-// -- Conectarse a un recurso de internet y obtener la respuesta
-
-// * El cliente envía una solicitud a un servidor
-// * El servidor procesa la solicitud y devuelve una respuesta
-
 public class Main{
     public static void main(String[] args) throws IOException, InterruptedException {
-
-        // Variable donde almacenaremos la respuesta
         String responseBody;
 
-        // Nuestra solicitud se hará al servidor lode.uno,
-        // le pediremos el recurso holamundo.txt"
+        // Ahora probaremos hacer la consulta al servidor
+        // de ExchangeRate que es una Web API.
+
+        // Documentación completa: https://www.exchangerate-api.com/docs/
+
+        // Una Web API tiene distintos endpoints.
+        // Cada endpoint corresponderá a una consulta distinta.
+
+        // Usaremos el endpoint /pair con el cual podremos consultar
+        // el ratio de conversión de las monedas que se le indiquen
+        // o la conversión de una cantidad determinada.
+
+        // Para el primer caso, ratio de conversión, usaremos
+        // GET https://v6.exchangerate-api.com/v6/CLAVE-DE-API/pair/MONEDA-1/MONEDA-2
+
+        // Información extra para ampliar:
+        //     GET indica el métod0 HTTP que habitualmente será GET o POST.
+        //     Las solicitudes GET pueden hacerse desde la barra de direcciones
+        //     del navegador web.
+
+        // Para el segundo caso, conversión de una cantidad determinada, usaremos
+        // GET https://v6.exchangerate-api.com/v6/CLAVE-DE-API/pair/MONEDA-1/MONEDA-2/CANTIDAD
+
+        // El endpoint, como puede verse, recibirá valores variables:
+        // -- CLAVE-DE-API
+        // -- MONEDA-1
+        // -- MONEDA-2
+        // -- CANTIDAD
+
+        // ¡Declarémoslas como variables en nuestro programa
+        // y asignémosle valores!
+
+        // A la clave de API la obtuvimos en https://app.exchangerate-api.com/sign-up
+        // Usemos esta, no es necesario crear otra.
+        String claveDeApi = "382e8d7a9bae4cf4c9c58bfa";
+
+        // Códigos: https://www.exchangerate-api.com/docs/supported-currencies
+        String moneda1 = "EUR"; // Código de euro.
+        String moneda2 = "USD"; // Código de dolar de EEUU.
+
+        String cantidad = "2"; // 2 euros a 2 dólares
+
         // Al siguiente String se lo pasaremos luego al
         // métod0 de HttpClient que construye un objeto
         // del tipo solicitud HttpRequest.
-        String url = "https://lode.uno/holamundo.txt";
+        String url = "https://v6.exchangerate-api.com/v6/"+claveDeApi+"/pair/"+moneda1+"/"+moneda2+"/"+cantidad;
+        // Expandiendo las variables la URL de la solicitud quedará como
+        // https://v6.exchangerate-api.com/v6/382e8d7a9bae4cf4c9c58bfa/pair/EUR/USD/2
+
 
         // El Cliente es el objeto que envía la consulta
         // como un objeto HttpRequest y recibe la respuesta
@@ -52,5 +87,28 @@ public class Main{
         responseBody = response.body();
 
         System.out.println(responseBody);
+        // Ahora obtendremos algo similar a:
+        // {"result":"success","documentation":"https://www.exchangerate-api.com/docs","terms_of_use":"https://www.exchangerate-api.com/terms","time_last_update_unix":1747440002,"time_last_update_utc":"Sat, 17 May 2025 00:00:02 +0000","time_next_update_unix":1747526402,"time_next_update_utc":"Sun, 18 May 2025 00:00:02 +0000","base_code":"EUR","target_code":"USD","conversion_rate":1.1169,"conversion_result":2.2338}
+        // Esto es un archivo Json, un formato de texto para intercambiar datos
+        // estructurados entre máquinas.
+        // Formateado lo veremos mejor:
+        /*
+        {
+            "result": "success",
+            "documentation": "https://www.exchangerate-api.com/docs",
+            "terms_of_use": "https://www.exchangerate-api.com/terms",
+            "time_last_update_unix": 1747440002,
+            "time_last_update_utc": "Sat, 17 May 2025 00:00:02 +0000",
+            "time_next_update_unix": 1747526402,
+            "time_next_update_utc": "Sun, 18 May 2025 00:00:02 +0000",
+            "base_code": "EUR",
+            "target_code": "USD",
+            "conversion_rate": 1.1169,
+            "conversion_result": 2.2338
+        }
+        */
+
+        // ¡ALLÍ ESTÁ LA CONVERSIÓN HECHA!
+        // en el valor de la clave 'conversion_result'
     }
 }
