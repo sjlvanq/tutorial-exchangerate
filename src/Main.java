a82354f028d986151f90f6e5c4b0eb1f7766c162
sjlvanq <sjlvanq@gmail.com>
2025-05-19 08:58:24 -0300

    initial commit 05-conexion-iii

--- src/Main.java
+++ src/Main.java
@@ -5,100 +5,80 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class Main{
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         String responseBody;
 
-        // Ahora probaremos hacer la consulta al servidor
-        // de ExchangeRate que es una Web API.
+        // Manejo de excepciones
+//
+//        El métod0 send() de HTTPClient nos
+//        obliga a hacer algo con las excepciones
+//        IOException y InterruptedException, que
+//        pueden ser lanzadas por diversos
+//        problemas en la conexión o en la
+//        obtención de los datos.
+//
+//        En versiones anteriores de este código
+//        las declarábamos en la firma del métod0
+//        main:
+//            public static void main(String[] args) throws IOException, InterruptedException
+//
+//        pero esto era sólo para evitar errores
+//        en la compilación.
+//
+//        Ese tipo de declaraciones hechas en la
+//        firma del métod0 puede ser útil cuando
+//        se delega el manejo de las excepciones
+//        a otra parte del programa, pero siendo
+//        main nuestro métod0 principal no hay a
+//        dónde delegar nada.
+//
+//        Lo ideal sería que si se produce algún
+//        error en la conexión, pueda presentarse
+//        al usuario un mensaje amigable
+//        informándolo.
+//
+//        Entonces vamos ahora a dotar al código
+//        de una estructura básica para hacerlo.
 
-        // Documentación completa: https://www.exchangerate-api.com/docs/
-
-        // Una Web API tiene distintos endpoints.
-        // Cada endpoint corresponde a una consulta distinta.
-
-        // Usaremos el endpoint /pair con el cual podremos consultar
-        // el ratio de conversión de las monedas que se le indiquen
-        // o la conversión de una cantidad determinada de dinero entre
-        // esas monedas.
-
-        // Para el primer caso, obtener el ratio de conversión, usaremos
-        // GET https://v6.exchangerate-api.com/v6/CLAVE-DE-API/pair/MONEDA-1/MONEDA-2
-
-        // Información extra para ampliar:
-        //     GET indica el métod0 HTTP requerido. Habitualmente será GET o POST.
-        //     Las solicitudes GET pueden hacerse desde la barra de direcciones
-        //     del navegador web.
-
-        // Para el segundo caso, convertir una cantidad determinada de dinero, usaremos
-        // GET https://v6.exchangerate-api.com/v6/CLAVE-DE-API/pair/MONEDA-1/MONEDA-2/CANTIDAD
-
-        // El endpoint, como puede verse, recibirá valores variables:
-        // -- CLAVE-DE-API
-        // -- MONEDA-1
-        // -- MONEDA-2
-        // -- CANTIDAD
-
-        // Declarémoslas como variables en nuestro programa
-
-        // A la clave de API la obtuvimos en https://app.exchangerate-api.com/sign-up
-        // Usemos esta, no es necesario crear otra.
         String claveDeApi = "382e8d7a9bae4cf4c9c58bfa";
 
-        // Códigos de monedas: https://www.exchangerate-api.com/docs/supported-currencies
-        String moneda1 = "EUR"; // Código de euro.
-        String moneda2 = "USD"; // Código de dolar de EEUU.
+        String moneda1 = "EUR";
+        String moneda2 = "USD";
+        String cantidad = "2";
 
-        String cantidad = "2"; // 2 euros a 2 dólares
-
-        // Ahora construiremos la URL usando estas variables
         String url = "https://v6.exchangerate-api.com/v6/"+claveDeApi+"/pair/"+moneda1+"/"+moneda2+"/"+cantidad;
-        // Expandiendo las variables la URL de la solicitud quedará como
-        // https://v6.exchangerate-api.com/v6/382e8d7a9bae4cf4c9c58bfa/pair/EUR/USD/2
 
+        // La forma try(recurso) se asegura de cerrar
+        // el objeto entre paréntesis se produzca o
+        // no algún error durante el proceso.
 
-        // Ya hemos visto qué es y qué hace el objeto Cliente.
-        HttpClient client = HttpClient.newHttpClient();
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .build();
 
-        // Ya hemos visto qué es y para qué se emplea el objeto Response.
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
 
-        // Enviamos la solicitud
-        HttpResponse<String> response = client
-            .send(request, HttpResponse.BodyHandlers.ofString());
+            responseBody = response.body();
+            System.out.println(responseBody);
 
-        // Cerramos el cliente
-        client.close();
-
-        // La parte de la respuesta que nos interesa aquí es el body().
-        responseBody = response.body();
-
-        System.out.println(responseBody);
-
-        // Esta vez obtendremos algo similar a:
-        // {"result":"success","documentation":"https://www.exchangerate-api.com/docs","terms_of_use":"https://www.exchangerate-api.com/terms","time_last_update_unix":1747440002,"time_last_update_utc":"Sat, 17 May 2025 00:00:02 +0000","time_next_update_unix":1747526402,"time_next_update_utc":"Sun, 18 May 2025 00:00:02 +0000","base_code":"EUR","target_code":"USD","conversion_rate":1.1169,"conversion_result":2.2338}
-        // Esto es un archivo Json, un formato de texto para intercambiar datos
-        // estructurados entre máquinas.
-
-        // Formateado:
-        /*
-        {
-            "result": "success",
-            "documentation": "https://www.exchangerate-api.com/docs",
-            "terms_of_use": "https://www.exchangerate-api.com/terms",
-            "time_last_update_unix": 1747440002,
-            "time_last_update_utc": "Sat, 17 May 2025 00:00:02 +0000",
-            "time_next_update_unix": 1747526402,
-            "time_next_update_utc": "Sun, 18 May 2025 00:00:02 +0000",
-            "base_code": "EUR",
-            "target_code": "USD",
-            "conversion_rate": 1.1169,
-            "conversion_result": 2.2338
+        } catch (IOException | InterruptedException e) {
+            System.err.println("Ocurrió un error durante la solicitud HTTP:");
+            System.out.println(e.getMessage());
+            //e.printStackTrace();
         }
-        */
 
-        // ¡Allí está la conversión hecha
-        // en el valor de la clave 'conversion_result'!
+        // __ A tener en cuenta:__
+//        No se producirá una excepción si, por
+//        ejemplo, se envía un código de moneda
+//        incorrecto. En ese caso no habrá habido
+//        ningún problema con la conexión y el
+//        manejo será distinto; bien se podrá
+//        consultar el encabezado de la respuesta,
+//        o bien mediante la clave result del Json
+//        recibido (para el caso específico de la
+//        API de ExchangeRate).
+
     }
 }
