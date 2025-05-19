7725aca8197c49dcab806d55710318b16ddb2e76
sjlvanq <sjlvanq@gmail.com>
2025-05-19 07:07:01 -0300

    initial commit 04-gson-ii

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,65 @@
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import dto.Compa;
 
 public class Main{
     public static void main(String[] args) {
-        // Nos hemos conectado a la Web API y hemos obtenido
-        // la información que necesitábamos.
+        // En este código sumamos un métod0 que nos será
+        // de utilidad para el caso que nos ocupa.
 
-        // Hay que avanzar hacia un programa que alguien
-        // pueda usar sin necesidad de modificar el código
-        // ni buscar la información que le interesa dentro
-        // de más código como es el Json que recibimos del
-        // servidor.
+//                      En el ejemplo anterior
+//        deserializamos un Json en un objeto
+//        cuyos atributos tenían el mismo nombre
+//        que las claves. Es decir, un objeto
+//        Json como {nombre: "elquino"}, donde
+//        nombre es la clave y elquino el valor,
+//        se mapeaba a un objeto que tenía el
+//        atributo privado nombre y el getter
+//        nombre().
+//
+//                Si revisamos la respuesta que
+//        obtenemos de la API de ExchangeRate en
+//        los primeros ejemplos, nos encontramos
+//        con un problema.
+//
+//                Ocurre que las claves de la
+//        respuesta tienen un formato que no es
+//        el recomendado para usar como nombres
+//        de atributos o variables en Java.
+//
+//                Encontramos claves como
+//        "time_last_update_utc", siendo que el
+//        estilo recomendado para Java sería
+//        timeLastUpdateUtc.
+//
+//            Cuando Gson intente deserializar a
+//        nuestro record esa información buscará
+//        un atributo con ese nombre... y no lo
+//        encontrará.
+//
+//        Afortunadamente podemos indicarle las
+//        equivalencias de una forma sencilla
+//        cuando el caso es transformar
+//        "snake_case" a "camelCase":
 
-        // Para esto último usaremos Gson
-        // Con Gson podemos transformar un Json
-        // { nombre: 'elquino', anio: 2025 }
-        // en un objeto de Java que nos permita, por ejemplo,
-        // mostrar en pantalla el valor de 'nombre' haciendo:
-        // System.out.println(compa.nombre);
+        // -------------------------------
+        // Ahora las claves del Json están en snake_case
+        // Nuestro DTO tiene los atributos en camelCase
+        String json = "{nombre_nombre: 'El Quino', anio_anio: 2025}";
 
-        String json = "{nombre: 'El Quino', anio: 2025}";
+        // LOWER_CASE_WITH_UNDERSCORES le dice a Gson que
+        // los nombres en el JSON están en snake_case y
+        // deben mapearse a atributos camelCase.
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
 
-        // Hemos definido un tipo de objeto al cual volcar
-        // esta información en src/dto/Compa.java
-
-        // Lo que sigue es, en principio, tan simple como hacer:
-        Gson gson = new Gson(); // Creamos un objeto de tipo Gson
         Compa elquino = gson.fromJson(json, Compa.class);
 
-        // Imprimimos el nombre
-        System.out.println(elquino.nombre());
+        System.out.println(elquino.nombreNombre());
+        System.out.println(elquino.anioAnio());
 
-        // El record Compa ha creado automáticamente
-        // un atributo privado nombre
-        // y un getter para ese atributo nombre()
-
-        // También lo ha hecho con el año
-        System.out.println(elquino.anio());
+        // ¡OK! Ahora ya sabemos cómo hay que definir
+        // el record para mapear la respuesta de la API.
 
     }
 }
--- src/dto/Compa.java
+++ src/dto/Compa.java
@@ -3,4 +3,4 @@ package dto;
 // Record para manejar un Json del tipo "{nombre: 'elquino', anio: 2025}"
 // Record es una clase inmutable a la que no es necesario
 // declararle explícitamente setters y getters.
-public record Compa (String nombre, int anio) { }
+public record Compa (String nombreNombre, int anioAnio) { }
